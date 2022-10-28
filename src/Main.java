186b2641dd54b9ad367f3671cec42a6c7667df49
unknown <BT_3W2_16@BKTECH.LOCAL>
2022-10-28 09:23:26 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int num = myScanner.nextInt();
+
+        if (num <= 0) {
+            System.out.println("That's not positive");
+        } else {
+            if ((num % 2) == 1) {
+                if ((num % 5) == 0) {
+                    System.out.println("Divisible by 5!");
+                } if ((num % 7) == 0) {
+                    System.out.println("Divisible by 7!");
+                } if ((num % 5) != 0 && (num % 7) != 0);
+            } else {
+                if (num > 1000) {
+                    System.out.println("Big even number!");
+                } else if (num > 100) {
+                    System.out.println("Medium even number!");
+                } else {
+                    System.out.println("Small even number!");
+                }
+            }
+        }
+    }
+}
